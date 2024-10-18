93120e9f4471fd186e89cf0d9de875125cca15f0
shyboy1044 <shyboy1044@proton.me>
2024-10-18 11:22:13 +0200

    repro

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,14 @@
-import { AppProps } from "$fresh/server.ts";
-import { Head } from "$fresh/runtime.ts";
+import { AppProps } from '$fresh/server.ts'
+import { Head } from '$fresh/runtime.ts'
 
 export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
         <link rel='stylesheet' href='/tailwind.css' />
-        <link rel="stylesheet" href="/app.css" />
+        <link rel='stylesheet' href='/app.css' />
       </Head>
       <Component />
     </>
-  );
+  )
 }
